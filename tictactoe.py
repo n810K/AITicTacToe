72056eaf9c8411e6e77a6a8719cd0a504dc60f93
Nathan <nathank1216@yahoo.com>
2023-04-07 23:35:41 -0700

    added main and winchecking

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-import random
+import random, math
 
 referenceBoard = [
     ["0","1","2"],
@@ -17,27 +17,55 @@ def printBoardFunction(board):
         print("This is the current state of the board:")
     for row in board:
         print(row)
+    print("")
 
 def boardWinCheck(board):
     #Check to see if row is a win
     for row in board:
         if (len(set(row)) == 1 and set(row) != {"-"}):
-            return True
+            if (set(row) == {"X"}):
+                return True, "X"
+            return True, "O"
 
     #check to see if column is a win
     for i in range(len(board)):
-        columnList = []
+        column = []
         for j in range(len(board[i])):
-            columnList.append(board[j][i])
-            if (len(set(columnList)) == 1 and set(columnList) != {"-"}):
-                return True
+            column.append(board[j][i])
+        if (len(set(column)) == 1 and set(column) != {"-"}):
+            if (set(column) == {"X"}):
+                return True, "X"
+            return True, "O"
+    
+    # check / diagonal
+    forwardDiagonal = [board[2][0], board[1][1], board[0][2]]
+    if (len(set(forwardDiagonal)) == 1 and set(forwardDiagonal) != {"-"}):
+        if (set(forwardDiagonal) == {"X"}):
+            return True, "X"
+        return True, "O"
+
+    #check \ diagonal 
+    backDiagonal = [board[0][0], board[1][1], board[2][2]]
+    if (len(set(backDiagonal)) == 1 and set(backDiagonal) != {"-"}):
+        if (set(backDiagonal) == {"X"}):
+            return True, "X"
+        return True, "O"
     
     return False
 
 def playerTurn(board):
     printBoardFunction(mainBoard)
-    playerSelectedSpace = int(input("Where would you like to play? (Refer to reference board if you forget the indices)"))
-    
+
+    playerSelectedSpace = int(input("Which spot would you like to play? (Refer to reference board if you forget the indices): "))
+    convertedSpace = [int(math.floor(playerSelectedSpace/3)),playerSelectedSpace%3]
+
+    while (mainBoard[convertedSpace[0]][convertedSpace[1]] != "-" and playerSelectedSpace > 8):
+        print("Invalid Space")
+        playerSelectedSpace = int(input("Which spot would you like to play? (Refer to reference board if you forget the indices): "))
+        convertedSpace = [int(math.floor(playerSelectedSpace/3)),playerSelectedSpace%3]
+
+    #print(convertedSpace)
+    mainBoard[convertedSpace[0]][convertedSpace[1]] = "X"
 
 def validSpaces(board):
     validSpaceList = []
@@ -62,19 +90,31 @@ def AITurn(board):
     #if difficultpass = true, selected space is chosen, if false, select a random space
 
 def main():
+    winCheck = False
+
     print("These are the coordinates you can play, and the corresponding index that you will use\n")
     printBoardFunction(referenceBoard)
 
     print("\n")
     printBoardFunction(mainBoard)
 
-    difficultyLevel = float(input("Enter a difficulty level from 0-100 (with 100 being impossible): "))/100
-    print(difficultyLevel)
+    difficultyLevel = float(input("Enter a difficulty level from 0-100 (with 100 being the hardest): "))/100
+
+    print("Selected difficulty: ", difficultyLevel,"\n")
+
+    print("You will be 'X', your opponent will be 'O'\n")
+    while (not winCheck):
 
     #Determine chance
-    playerTurn(mainBoard)
+        playerTurn(mainBoard)
+        #printBoardFunction(mainBoard)
+        winCheck = boardWinCheck(mainBoard)
+        print("YUH", winCheck)
 
 
+    print("OVER")
+    print("Final State of Board:")
+    printBoardFunction(mainBoard)
 
 
 if __name__ == "__main__":
